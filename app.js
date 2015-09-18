a359c009edf5c4d455b5dc41b2221fab51e8f75b
Paul Joannon <hello@pauljoannon.com>
2015-09-18 15:29:13 +0200

    remove scale change

--- app.js
+++ app.js
@@ -26,19 +26,13 @@ $(function() {
         var current = data[0];
 
         var minDate = _.min(_.pluck(data, 'date'), function(d) { return d.unix(); }),
-            midDate = moment('01/05/2015', 'DD/MM/YYYY'),
             maxDate = _.max(_.pluck(data, 'date'), function(d) { return d.unix(); }),
             totalDays = maxDate.diff(minDate, 'days');
 
         var range = { },
-            scale1 = d3.scale.linear().domain([minDate.unix(), maxDate.unix()]).range([0, 10]),
-            scale2 = d3.scale.linear().domain([midDate.unix(), maxDate.unix()]).range([10, 100]);
+            scale = d3.scale.linear().domain([minDate.unix(), maxDate.unix()]).range([0, 100]);
         _.each(data, function(d, i) {
-            if (d.date.isBefore(midDate)) {
-                d.percent = _.round(scale1(d.date.unix()), 2);
-            } else {
-                d.percent = _.round(scale2(d.date.unix()), 2);
-            }
+            d.percent = _.round(scale(d.date.unix()), 2);
 
             // Slider
             var name = i === 0 ? 'min'
